d9ef74ff4d3598b02b20808e46425f5b2f79819b
littlewall <mz@milanzitka.cz>
2024-07-11 22:03:12 +0200

    feat: add comming soon page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,99 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import {ReactNode, useEffect, useRef, useState} from "react";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+const random = (max: number) => Math.floor(Math.random() * max);
+
+const generateStars = () => {
+    const stars = [];
+
+    const starCount = Math.floor(Math.random() * 200 + 700);
+
+    for (let i = 0; i < starCount; i++) {
+        const size = `${Math.random() * 3 + 1}px`;
+
+        stars.push(
+            <div
+                key={`starryNightStar${i}`}
+                className={styles.star}
+                style={{
+                    top: `${Math.random() * 200}%`,
+                    left: `${Math.random() * 200}%`,
+                    width: size,
+                    height: size,
+                    "--pulse-delay": `${Math.random() * 5}s`,
+                } as React.CSSProperties}
             />
-          </a>
-        </div>
-      </div>
+        );
+    }
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    return stars;
+};
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+const Home = () => {
+    const shootingStarsRef = useRef<HTMLDivElement>(null);
+    const [generatedStars, setGeneratedStars] = useState<ReactNode[]>([]);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+    const removeShootingStar = (shootingStarElement: HTMLDivElement) => {
+        if (!shootingStarsRef.current) {
+            return;
+        }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+        shootingStarsRef.current.removeChild(shootingStarElement);
+    };
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    useEffect(() => {
+        const shootingStars = shootingStarsRef.current;
+
+        if (!shootingStars || shootingStars.children.length > 0) {
+            return;
+        }
+
+        const generateShootingStar = () => {
+            const newShootingStar = document.createElement("div");
+
+            newShootingStar.classList.add(styles.shootingStar);
+            newShootingStar.style.setProperty("--left", `calc(50% - ${random(500) - 200}px)`);
+            newShootingStar.style.setProperty("--top", `calc(50% - ${random(500) - 200}px)`);
+
+            shootingStars.appendChild(newShootingStar);
+
+            setTimeout(() => {
+                removeShootingStar(newShootingStar);
+                generateShootingStar();
+            }, 8000);
+        };
+
+        generateShootingStar();
+    }, [shootingStarsRef.current]);
+
+    useEffect(() => {
+        setGeneratedStars(generateStars());
+    }, []);
+
+    return (
+        <main className={styles.main}>
+            <div className={styles.starrySky}>
+                <div className={styles.center}>
+                    <Image
+                        className={styles.logo}
+                        src="/question-mark.svg"
+                        alt="Co když..? - Muzikál logo"
+                        width={300}
+                        height={300}
+                        priority
+                    />
+                    <h1 className={styles.title}>Co když..?</h1>
+                    <h2 className={styles.subtitle}>nový český muzikál</h2>
+                </div>
+                <div className={styles.stars}>
+                    {generatedStars}
+                </div>
+                <div className={styles.shootingStars} ref={shootingStarsRef}></div>
+            </div>
+        </main>
+    );
 }
+
+export default Home;
